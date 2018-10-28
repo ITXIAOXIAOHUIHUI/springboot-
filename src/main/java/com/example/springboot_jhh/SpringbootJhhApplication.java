51ad9a6e67942bad7f421e10a0f903d59bc52ba9
jianghaihui <1070408931@qq.com>
2018-10-28 14:21:36 +0800

    提交所有文件

---
+++ src/main/java/com/example/springboot_jhh/SpringbootJhhApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_jhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJhhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJhhApplication.class, args);
+    }
+}
